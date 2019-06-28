218ad50793291a10cb0831f3e8cbe8851bc439cb
Bulzan Stefan <stefan.bulzan@nordlogic.com>
2019-06-29 01:55:39 +0300

    finished course 2

--- src/main/java/ro/itschool/curs2/Arrays.java
+++ src/main/java/ro/itschool/curs2/Arrays.java
@@ -1,7 +1,35 @@
 package ro.itschool.curs2;
 
 /**
- *
+ * Arrays are a collection of elements of a specific type
  */
 public class Arrays {
+    public static void main(String[] args) {
+        //Defining an array
+        //On the type
+        int[] array;
+
+        //Intializing an array:
+        //You can initialize an array by specifying its length. The array element will all be the java default for that type
+        array = new int[10];
+
+        //You can initialize the array by specifying its content. No length should be specified!
+        array = new int[]{1, 2, 4, 5};//this will initialize an array with length 4 that contains 1,2,4,5
+
+        //You can define and intialize the array in the same line:
+        double[] initializedArray = new double[]{5, 6, 7.1, 3.14, 3.4};
+
+        //Accessing the element from an array
+        //To access the element from an array you need to specify the desired position between [ and ]
+        System.out.println("In order to access the 2nd element from array array you write array[1] " + array[1]);
+        //NOTE: the first element in an array will be on position 0. The last one on position length-1
+
+        //Change a value in an array:
+        //In order to change a value in an array, you need to access that element and assign a new value
+        array[2] = 55;
+        System.out.println("Now the value of array[2] is " + array[2]);
+
+        //Length of an array: can be retrieved with array.length
+        System.out.println("The length of the array is " + array.length);
+    }
 }
--- src/main/java/ro/itschool/curs2/JavaPrimitiveTypes.java
+++ src/main/java/ro/itschool/curs2/JavaPrimitiveTypes.java
@@ -15,6 +15,7 @@ public class JavaPrimitiveTypes {
         //we have primitive types for all important data types:
         //------------------------------------------------------------------------------------------
         //byte : occupies 1 byte; used for small values between -128 and 128
+        //default value = 0
         byte byteVar = 127;
         System.out.println("Byte var is " + byteVar);
         //if we assign value outside the limits a compilation error will occur
@@ -22,40 +23,47 @@ public class JavaPrimitiveTypes {
 
         //------------------------------------------------------------------------------------------
         //short : occupies 2 bytes: values between -32,768 and 32,767
+        //default value = 0
         short shortVar = 30_000;
         System.out.println("The short var is " + shortVar);
 
         //------------------------------------------------------------------------------------------
         //int: occupies 4 bytes: values between -2,147,483,648 and 2,147,483,647
         //this is the most used primitive in java. Other than the cases when you explicitly want to use byte or short, use this one
+        //default value = 0
         int intVar = 213;
         System.out.println("The int value is " + intVar);
 
         //------------------------------------------------------------------------------------------
         //long: occupies 8 bytes: values between -9,223,372,036,854,775,808 and 9,223,372,036,854,775,807
         // use this when you think int won't be enough.
+        //default value = 0
         long longVar = 1328094381;
         System.out.println("The long value is " + longVar);
 
         //------------------------------------------------------------------------------------------
         //float: occupies 4 bytes :  values between 3.4e−038 and 3.4e+038
         //used for decimal low numbers. By default the decimal literal is double, so you have to specify you want a float by adding F at the end of the number
+        //default value = 0
         float floatVar = 444.51F;
         System.out.println("Float var is " + floatVar);
 
         //------------------------------------------------------------------------------------------
         //double: occupies 8 bytes :  values between 1.7e−308 and 1.7e+038
         //this type is used usually for numbers with decimals. Use float only if you have specific reasons to do it
+        //default value = 0
         double doubleVar = 444.51;
         System.out.println("Double var is " + doubleVar);
 
         //------------------------------------------------------------------------------------------
         //double: occupies 1 bit: true or false
+        //default value = false
         boolean booleanVar = true;
         System.out.println("Boolean var is " + booleanVar);
 
         //------------------------------------------------------------------------------------------
         //char: occupies 2 bytes: value that represents a character
+        //default value = 0
         char charVar = 'A';
         System.out.println("Char value is " + charVar);
         charVar = 123;
